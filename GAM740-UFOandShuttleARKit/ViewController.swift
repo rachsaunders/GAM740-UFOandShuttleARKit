e8518671fb8b5569e408d476f1623e690c1ae8fe
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-12-18 08:26:25 +0000

    Ufo goes left to right whilst going upwards

--- GAM740-UFOandShuttleARKit/ViewController.swift
+++ GAM740-UFOandShuttleARKit/ViewController.swift
@@ -11,7 +11,13 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    
+    // OUTLETS
     @IBOutlet var sceneView: ARSCNView!
+    
+    // VARS
+    
+    var chosenNode: SCNNode?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +29,148 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        
+        registerGestureRecognizer()
+        
+        
+        
     }
+    
+    func registerGestureRecognizer() {
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap))
+        
+        tap.numberOfTapsRequired = 1
+        doubleTap.numberOfTapsRequired = 2
+        
+        sceneView.addGestureRecognizer(tap)
+        sceneView.addGestureRecognizer(doubleTap)
+        
+    }
+    
+    @objc func handleTap(gestureRecognizer: UIGestureRecognizer) {
+        
+        guard let vehicle = chosenNode else {
+            return
+        }
+        
+        switch vehicle.name {
+        case "ufo"?:
+            return ufoAction()
+        case "shuttle"?:
+            return shuttleAction()
+        default: ufoAction()
+        }
+        
+    }
+    
+    
+    @objc func handleDoubleTap(gestureRecognizer: UIGestureRecognizer) {
+        guard let name = chosenNode?.name else {
+            return
+        }
+        if name == "ufo" {
+            launchUfo()
+        } else {
+            return
+        }
+        
+        
+    }
+    
+    func addShuttle() {
+        
+        let scene = SCNScene(named: "art.scnassets/Shuttle/shuttle.dae")!
+        guard let fireNode = scene.rootNode.childNode(withName: "fireNode", recursively: true),
+              let shuttleNode = scene.rootNode.childNode(withName: "shuttleNode", recursively: true)
+              
+              else {
+            fatalError("Something is missing")
+            }
+        
+        
+        let fire = SCNParticleSystem(named: "fire.scnp", inDirectory: nil)
+        
+        fireNode.addParticleSystem(fire!)
+        shuttleNode.addChildNode(fireNode)
+        
+        shuttleNode.scale = SCNVector3(0.3,0.3,0.3)
+        
+        // fixes error of position
+        shuttleNode.position = SCNVector3(x: 0, y: -0.5, z: -1.5)
+        
+        sceneView.scene.rootNode.addChildNode(shuttleNode)
+        
+        shuttleNode.name = "shuttle"
+        chosenNode = shuttleNode
+        
+        
+    }
+    
+    
+    func addUfo(){
+        let scene = SCNScene(named: "art.scnassets/UFO/Ufo.dae")
+        guard let ufoNode = scene?.rootNode.childNode(withName: "ufoNode", recursively: true) else {
+            fatalError("Something is missing")
+        }
+        
+        let stars = SCNParticleSystem(named: "stars.scnp", inDirectory: nil)
+        
+        let starNode = SCNNode()
+        starNode.addParticleSystem(stars!)
+        
+        ufoNode.addChildNode(starNode)
+        
+        ufoNode.scale = SCNVector3(0.3,0.3,0.3)
+        
+        // fixes error of position
+        ufoNode.position = SCNVector3(x: 0, y: -0.5, z: -0.5)
+        
+        sceneView.scene.rootNode.addChildNode(ufoNode)
+        
+        ufoNode.name = "ufo"
+        chosenNode = ufoNode
+        
+    }
+    
+    func shuttleAction() {
+        guard let node = chosenNode else {
+            return
+        }
+        let upAction = SCNAction.move(by: SCNVector3(x: 0, y: 100, z: 0.05), duration: 100)
+        node.runAction(SCNAction.repeatForever(upAction))
+    }
+    
+    func ufoAction() {
+        guard let node = chosenNode else {
+            return
+        }
+        let leftAction = SCNAction.move(by: SCNVector3(-1,0,0), duration: 1)
+        let rightAction = SCNAction.move(by: SCNVector3(1,0,0), duration: 1)
+        
+        leftAction.timingMode = SCNActionTimingMode.easeInEaseOut
+        rightAction.timingMode = SCNActionTimingMode.easeInEaseOut
+        
+        let rotate = SCNAction.rotateBy(x: 0, y: CGFloat(0.2), z: 0, duration: 0.1)
+        
+        node.runAction(SCNAction.repeatForever(SCNAction.sequence([leftAction,rightAction])))
+        node.runAction(SCNAction.repeatForever(rotate))
+    }
+    
+    func launchUfo() {
+        guard let node = chosenNode else {
+            return
+        }
+        let upAction = SCNAction.move(by: SCNVector3(100,100,0), duration: 100)
+        node.runAction(SCNAction.repeatForever(upAction))
+    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -71,4 +214,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    
+    // IBACTIONS
+    
+    @IBAction func shuttleButton(_ sender: Any) {
+        addShuttle()
+    }
+    
+    
+    @IBAction func ufoButton(_ sender: Any) {
+        addUfo()
+        
+    }
+    
 }
